d8ce307b692107c2355fc12e8126755b8781ee87
Sam Makhlouf <maksam7dev@gmail.com>
2023-10-19 11:35:20 +0000

    added editor

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import Cover from "@/components/cover"
+import Editor from "@/components/editor"
 import Toolbar from "@/components/toolbar"
 import { Skeleton } from "@/components/ui/skeleton"
 import { api } from "@/convex/_generated/api"
@@ -49,6 +50,8 @@ export default function DocumentPage({ params }: DocumentPageProps) {
 
       <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
         <Toolbar initialData={doc} />
+
+        <Editor onChange={()=>{}} initialContent={doc.content} />
       </div>
     </div>
   )
