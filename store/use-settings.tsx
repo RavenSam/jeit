f939f99b2cc467a39798010bfac9e9d457d13a49
Sam Makhlouf <maksam7dev@gmail.com>
2023-10-20 15:17:16 +0000

    persist width settings to local storage

--- store/use-settings.tsx
+++ store/use-settings.tsx
@@ -1,13 +1,15 @@
 import { create } from "zustand"
 
+const LOCAL_SETTINGS_KEY = "jet_settings"
+
 interface SettingsState {
   isOpen: boolean
   onOpen: () => void
   onClose: () => void
   toggle: () => void
-  
+
   editorWidth: number
-  setEditorWidth: (width:number) => void
+  setEditorWidth: (width: number) => void
 }
 
 export const useSettings = create<SettingsState>()((set, get) => ({
@@ -16,7 +18,10 @@ export const useSettings = create<SettingsState>()((set, get) => ({
   onClose: () => set({ isOpen: false }),
   toggle: () => set({ isOpen: !get().isOpen }),
 
+  editorWidth: Number(localStorage.getItem(LOCAL_SETTINGS_KEY)) || 896,
+  setEditorWidth: (width: number) => {
+    localStorage.setItem(LOCAL_SETTINGS_KEY, width.toString())
 
-  editorWidth:896,
-  setEditorWidth: (width:number) => set({ editorWidth:width }),
+    set({ editorWidth: width })
+  },
 }))
